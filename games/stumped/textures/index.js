65d15205b9fbe3e1ec2a1760c5c290e6d4c1daab
AHK <akck9@mst.edu>
2017-03-06 17:34:55 -0600

    Ground sprite

--- games/stumped/textures/index.js
+++ games/stumped/textures/index.js
@@ -1,5 +1,8 @@
 module.exports = {
     //<<-- Creer-Merge: settings -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
-    //"example": "example.png",
+
+    // Tiles
+    "ground": "ground.png",
+
     //<<-- /Creer-Merge: settings -->>
 };
