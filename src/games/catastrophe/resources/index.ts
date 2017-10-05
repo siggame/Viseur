577f6fc27b2a71e1cebdf3dbb0b1604674ec4124
Elijah Kelly <elk8kh@mst.edu>
2017-10-05 16:33:58 -0500

    Completed indexing for assets

--- src/games/catastrophe/resources/index.ts
+++ src/games/catastrophe/resources/index.ts
@@ -2,6 +2,15 @@ import { createResources, load } from "src/viseur/renderer";
 
 export const GameResources = createResources("Catastrophe", {
     // <<-- Creer-Merge: resources -->>
-    // "example": load("example.png"),
+    builderHuman: load("BuilderHuman.png"),
+    freshHuman: load("FreshHuman.png"),
+    friendlyHuman: load("FriendlyHuman.png"),
+    soldierUnit: load("soldier.png"),
+    converterUnit: load("converter.png"),
+    catOverlord: load("overlord.png"),
+
+    groundTile: load("groundTile.png"),
+    shelterTile: load("shelterTile.png"),
+    monumentTile: load("monument.png"),
     // <<-- /Creer-Merge: resources -->>
 });
