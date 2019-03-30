9e4b30b49dd43b9b179479b62db074b3a647eb80
Alan <atwpb@mst.edu>
2019-03-29 21:25:24 -0500

    Fix bug in index.ts

--- src/games/stardash/resources/index.ts
+++ src/games/stardash/resources/index.ts
@@ -3,10 +3,8 @@ import { createResources, load } from "src/viseur/renderer";
 /** These are the resources (sprites) that are loaded and usable by game objects in Stardash. */
 export const GameResources = createResources("Stardash", {
     // <<-- Creer-Merge: resources -->>
-        background: load("spacebackground.png"),
-        width: 16,
-        height: 9,
-    }),
+    background: load("spacebackground.png"),
+    
     sun: load("sun.png"),
     earth_planet: load("earth_planet.png"),
     alien_planet: load("alien_planet.png")
