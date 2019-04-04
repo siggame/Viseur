abf138f5d199c30feabffbafbced9f4595e90999
Strahm, Jeffrey S.(S&T-Student) <jssfg8@mst.edu>
2019-04-04 01:06:20 -0500

    Updated documentation

--- src/games/stardash/resources/index.ts
+++ src/games/stardash/resources/index.ts
@@ -3,11 +3,7 @@ import { createResources, load } from "src/viseur/renderer";
 /** These are the resources (sprites) that are loaded and usable by game objects in Stardash. */
 export const GameResources = createResources("Stardash", {
     // <<-- Creer-Merge: resources -->>
-    background: load("spacebackground.png"),
-    
-    sun: load("sun.png"),
-    earth_planet: load("earth_planet.png"),
-    alien_planet: load("alien_planet.png"),
-    asteroid: load("asteroid.png")
+    test: load("test.png"), // load files like this,
+                            // and remember to remove these lines and file!
     // <<-- /Creer-Merge: resources -->>
 });
--- src/games/stardash/state-interfaces.ts
+++ src/games/stardash/state-interfaces.ts
@@ -11,7 +11,9 @@ import { IBaseGame, IBaseGameObject, IBasePlayer } from "@cadre/ts-utils/cadre";
  */
 export interface IGameState extends IBaseGame {
     /**
-     * All the celestial bodies in the game.
+     * All the celestial bodies in the game. The first two are planets and the
+     * third is the sun. The fourth is the VP asteroid. Everything else is
+     * normal asteroids.
      */
     bodies: IBodyState[];
 
@@ -64,8 +66,7 @@ export interface IGameState extends IBaseGame {
     legendariumValue: number;
 
     /**
-     * The highest amount of material, barring rarity, that can be in a
-     * asteroid.
+     * The highest amount of material, that can be in a asteroid.
      */
     maxAsteroid: number;
 
@@ -75,8 +76,7 @@ export interface IGameState extends IBaseGame {
     maxTurns: number;
 
     /**
-     * The smallest amount of material, barring rarity, that can be in a
-     * asteroid.
+     * The smallest amount of material, that can be in a asteroid.
      */
     minAsteroid: number;
 
@@ -211,7 +211,7 @@ export interface IBodyState extends IGameObjectState {
     materialType: string;
 
     /**
-     * The Player that owns and can control this Unit.
+     * The Player that owns and can control this Body.
      */
     owner: IPlayerState;
 
@@ -387,7 +387,7 @@ export interface IProjectileState extends IGameObjectState {
     fuel: number;
 
     /**
-     * The Player that owns and can control this Unit.
+     * The Player that owns and can control this Projectile.
      */
     owner: IPlayerState;
 
