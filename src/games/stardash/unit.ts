45f8558bbc3121c5f2b5c6d870de8784d0df3312
Strahm, Jeffrey S.(S&T-Student) <jssfg8@mst.edu>
2019-04-04 01:13:29 -0500

    Revert "Added some constants and fixed functions." --merge flag is important.
    This reverts commit c5bb8f2ca030a2ed79c3e7b8445947e2deec99df.

--- src/games/stardash/unit.ts
+++ src/games/stardash/unit.ts
@@ -184,8 +184,8 @@ export class Unit extends makeRenderable(GameObject, SHOULD_RENDER) {
      * from the server. - The returned value is True if pathable by this unit,
      * false otherwise.
      */
-    public safe(x: number, y: number, callback?: (returned: boolean) => void): void {
-        this.runOnServer("safe", {x, y}, callback);
+    public open(x: number, y: number, callback?: (returned: boolean) => void): void {
+        this.runOnServer("open", {x, y}, callback);
     }
 
     /**
