23105bafa52b265233a3f3c73fa882d0d380f49a
Will Lynch <48072102+Vilhelm1@users.noreply.github.com>
2019-04-06 14:07:24 -0500

    Added load for different asteroids

--- src/games/stardash/resources/index.ts
+++ src/games/stardash/resources/index.ts
@@ -8,6 +8,9 @@ export const GameResources = createResources("Stardash", {
     sun: load("sun.png"),
     earth_planet: load("earth_planet.png"),
     alien_planet: load("alien_planet.png"),
-    asteroid: load("asteroid.png")
+    genarium: load("genarium.png"),
+    rarium: load("rarium.png"),
+    legendarium: load("legendarium.png"),
+    mythicite: load("mythicite.png")
     // <<-- /Creer-Merge: resources -->>
 });
