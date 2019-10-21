0c23601657518a15796af0e4f5042e8915dd8b68
Alan Truong <atwpb@mst.edu>
2019-10-21 16:49:24 -0500

    now render the correct structure

--- src/games/catastrophe/structure.ts
+++ src/games/catastrophe/structure.ts
@@ -76,9 +76,6 @@ export class Structure extends makeRenderable(GameObject, SHOULD_RENDER) {
         if (state.type === "monument") {
             this.sprite = this.addSprite.monument();
         }
-
-        
-
         this.container.position.set(state.tile.x, state.tile.y);
         // <<-- /Creer-Merge: constructor -->>
     }
